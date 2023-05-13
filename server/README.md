8bed80f3cd578cc37d33abdb69e7dea71206c02b
vincent-guizot <vincentguizot128@gmail.com>
2023-05-13 13:55:38 +0700

    server pg dan sequelize

--- server/README.md
+++ server/README.md
@@ -1,1 +1,32 @@
 # server
+
+## Database configuration with Sequelize
+
+1. Membuat konfigurasi sequelize dan pg awal
+
+```
+npx sequelize-cli init
+```
+
+2. Setting `config.json`
+
+3. Membuat database
+
+```
+npx sequelize-cli db:create
+```
+
+4. Membuat migration dan table
+
+```
+npx sequelize-cli model:generate --name User --attributes username:string,password:string,email:string,image:string,role:string
+
+npx sequelize-cli model:generate --name Item --attributes name:string,category:string,price:integer,stock:integer,image:string,UserId:integer
+
+```
+
+5. Migrate dari migrations
+
+```
+npx sequelize-cli db:migrate
+```
---
+++ server/models/item.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Item extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Item.init({
+    name: DataTypes.STRING,
+    category: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    stock: DataTypes.INTEGER,
+    image: DataTypes.STRING,
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Item',
+  });
+  return Item;
+};
